dc1746d1d6ef2ffab5b06486e613a699e9913d16
DaniilSh23 <danyasevas11@gmail.com>
2023-10-15 19:09:23 +0300

    поправил функционал получения ID чата, в связи с новшествами телеги

--- tgbot/handlers/group_chat_handlers.py
+++ tgbot/handlers/group_chat_handlers.py
@@ -1,9 +1,11 @@
+from django.urls import reverse
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from tag_bot.settings import MY_LOGGER
+from tag_bot.settings import MY_LOGGER, BASE_HOST, BOT_TOKEN
 from tgbot.db_work import get_group_detail
 from tgbot.filters.group_chat_filters import get_cmnd_for_check_perm_filter, tag_all_filter, filter_by_group_id
+from tgbot.keyboards.bot_keyboards import form_webapp_kbrd
 from tgbot.utils.client_actions import send_tag_msg
 
 
@@ -78,52 +80,15 @@ async def tag_all_handler(client: Client, update: Message):
         )
 
 
-@Client.on_message(filters.command(['test']) | filters.me & filters.bot & get_cmnd_for_check_perm_filter)
-async def get_command_for_check_perm(client: Client, update: Message):
-    """
-    Хэндлер для проверки прав бота в групповом чате.
-    """
-    # Get members
-    chat_id = '-1001854849400'
-    # async for member in client.get_chat_members(chat_id):
-    #     print(member)
-
-    # Получаем членов
-    members_lst = [member async for member in client.get_chat_members(chat_id)]
-    print(members_lst[0].user.username)
-
-    msg_str = ''
-    for i_member in members_lst:
-        if i_member.user.username:
-            msg_str = f"{msg_str} @{i_member.user.username}"
-    await client.send_message(
-        chat_id=chat_id,
-        text=f"{msg_str} хуй.\n\n\nСпасибо за внимание."
-    )
-
-    # TODO: это не дописано. Чет хуета была. Почему-то бот не мог получить инфу о чате, даже в котором состоит как
-    #  админ, получал ошибку Telegram says: [400 BOT_METHOD_INVALID] ... ХЗ, что делать, опускаю этот шаг пока что
-    MY_LOGGER.info(f'Апдейт в хэндлере бота get_command_for_check_perm')
-
-    # # _, group_lnk = update.text.split()
-    # group_lnk = 'https://t.me/+1ZKtuEk_ivk2NmZi'
-    # # group_hash = group_lnk.split('/')[-1]
-    # print(group_lnk)
-    # group_chat_obj = await client.get_chat(group_lnk)
-    # print(group_chat_obj)
-
-
-@Client.on_message(filters.private)
+@Client.on_message(filters.group & filters.command(commands=['id']))
 async def return_chat_id(_, update: Message):
     """
-    Метод, который возвращает ID чата, если в бота было переслано сообщение из него.
+    Метод, который возвращает ID чата, если была выполнена команда /id в групповом чате.
     """
-    MY_LOGGER.info(f'Получен апдейт на хэндлер возврата ID чата из которого переслано сообщение')
+    MY_LOGGER.info(f'Получен апдейт на хэндлер возврата ID чата.')
 
-    if update.forward_from_chat:
-        await update.reply_text(text=f'🆔 ID чата, из которого переслано это сообщение:\n\n'
-                                     f'<code>{update.forward_from_chat.id}</code>\n\n'
-                                     f'➕ Вы можете использовать его <b>для подключения собственной группы.</b>')
-    else:
-        await update.reply_text(text='🆔 Я могу предоставить ID чата, из которого Вы мне перешлете сообщение.\n\n'
-                                     '✖️ Это сообщение не было переслано из другого чата.')
+    # Отправляем ID данного чата
+    await update.reply_text(text=f'🆔 <b>ID данного чата:</b>\n\n'
+                                 f'<code>{update.chat.id}</code>\n\n'
+                                 f'➕ Он понадобиться, чтобы <b>подключить этот чат для тега пользователей</b>.\n\n'
+                                 f'🗑 Вы можете <b>удалить это сообщение</b>, чтобы иные участники чата его не видели.')
